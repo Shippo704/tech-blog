1e72c6d673737b5eb6fb14015e3554984df987bb
Shippo704 <matt.taylor704@yahoo.ca>
2023-10-07 16:39:58 -0400

    set up router for api routes in controllers/api/index.js

---
+++ controllers/api/index.js
@@ -0,0 +1,13 @@
+// import required modules and routes
+const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes');
+
+// set up the routes
+router.use('/comments', commentRoutes);
+router.use('./posts', postRoutes);
+router.use('./users', userRoutes);
+
+// export the router
+module.exports = router;
