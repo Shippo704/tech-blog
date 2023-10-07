c645850610ed7ea160cf97f10e9a99dab6a97a4f
Shippo704 <matt.taylor704@yahoo.ca>
2023-10-07 14:20:48 -0400

    initial commit. set up config/connection and server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+// required packages
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers.js');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up session
+const sess =  {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// Use session
+app.use(session(sess));
+
+// Create the Handlebars.js engine object with custom helper functions
+const hbs = exphbs.create({ helpers });
+
+// Inform Express.js which template engine we're using
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
