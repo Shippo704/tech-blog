7ed275fe83348879e51d3641412b39f5ef34e53a
Shippo704 <matt.taylor704@yahoo.ca>
2023-10-08 01:33:23 -0400

    fixed auth bug

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -69,7 +69,7 @@ router.post('/login', async (req, res) => {
             req.session.user_id = userData.id;
             req.session.loggedIn = true;
 
-            res.json({user: userData, message: 'Login successful'})
+            res.status(200).json({user: userData, message: 'Login successful'})
         });
     }
     // catch all errors
--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,7 @@
 // middleware function to check if user is logged in
 const loggedIn = (req, res, next) => {
     // if user is not logged in, redirect to login page
-    if (!req.session.logged_in) {
+    if (!req.session.loggedIn) {
         res.redirect('/login');
     }
     else {
