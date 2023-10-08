9efca53049cee86ac7314dc0feefa3d4db9e36cc
Shippo704 <matt.taylor704@yahoo.ca>
2023-10-07 21:19:22 -0400

    fixed error in connection.js and dotenv file

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
-require("dotenv").config();
+// use dotenv information
+require('dotenv').config();
 
-const Sequelize = require("sequelize");
+// require sequelize module
+const Sequelize = require('sequelize');
 
+// new instance of sequelize using jawsDB if available
+// otherwise use dotenv information
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: "localhost",
-      dialect: "mysql",
+      host: 'localhost',
+      dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
